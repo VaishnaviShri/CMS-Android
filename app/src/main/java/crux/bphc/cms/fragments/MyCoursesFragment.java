248a3bbd4a032463e4870420af4fa81b68bc7401
VaishnaviShri <vaishnavi.shri2019@gmail.com>
2020-05-12 17:40:22 +0530

    Add coordinate layout - dependecies issue

--- app/src/main/java/crux/bphc/cms/fragments/MyCoursesFragment.java
+++ app/src/main/java/crux/bphc/cms/fragments/MyCoursesFragment.java
@@ -98,7 +98,7 @@ public class MyCoursesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_my_courses, container, false);
+         return inflater.inflate(R.layout.fragment_my_courses, container, false);
     }
 
     @Override
